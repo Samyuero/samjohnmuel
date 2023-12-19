be13e8f1b340484b1a172ad014e129d25d1b6aae
samjohnmuel <154299319+samjohnmuel@users.noreply.github.com>
2023-12-19 20:05:27 +0800

    Delete webdev.js

--- webdev.js
+++
@@ -1,8 +0,0 @@
-<script src="ken.js"></script>
-const wrapper = document.querySelector('.wrapper');
-function registerActive() {
-    wrapper.classList.toggle('active');
-}
-function loginActive() {
-    wrapper.classList.toggle('active');
-}
