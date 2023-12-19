a98bab7cdcd1020f7b6b420352c64a4733cdd951
samjohnmuel <154299319+samjohnmuel@users.noreply.github.com>
2023-12-19 12:28:15 +0800

    Create webdev.js

---
+++ webdev.js
@@ -0,0 +1,8 @@
+<script src="ken.js"></script>
+const wrapper = document.querySelector('.wrapper');
+function registerActive() {
+    wrapper.classList.toggle('active');
+}
+function loginActive() {
+    wrapper.classList.toggle('active');
+}
